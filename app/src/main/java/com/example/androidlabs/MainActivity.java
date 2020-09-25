fb795d4f04aa1d4aa767e7e5cd4dd13f9a6a837e
Wilker Fernandes de Sousa <fern0097@algonquinlive.com>
2020-09-25 14:48:55 -0400

    Lab02 LinearLayout

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
 
         button.setOnClickListener(v -> {});
+
+        EditText theText = findViewById(R.id.editText);
+        String userTyped = theText.getText().toString();
 
     }
 }
