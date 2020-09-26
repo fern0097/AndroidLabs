2ec36be5ec85be9bcb67971aea60af30bc11615a
Wilker Fernandes de Sousa <fern0097@algonquinlive.com>
2020-09-25 21:30:32 -0400

    Step1

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,13 +3,10 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
-
 import com.google.android.material.snackbar.Snackbar;
 
 
@@ -18,33 +15,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
 
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(v -> {
-//                Toast.makeText(MainActivity.this, R.string.toast_message, Toast.LENGTH_SHORT).show();
+//
             Toast.makeText(MainActivity.this, getResources().getString(R.string.toast_message), Toast.LENGTH_SHORT).show();
         });
 
-        Switch mySwitch = findViewById(R.id.switch5);
+        final Switch mySwitch = findViewById(R.id.switch5);
+
         mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     Snackbar sb = Snackbar.make(MainActivity.this.findViewById(R.id.rootLayout), getResources().getString(R.string.switch_on), Snackbar.LENGTH_LONG);
-                    sb.setAction("Undo", v -> mySwitch.setChecked(false));
+                    sb.setAction(getResources().getString(R.string.undo), v -> mySwitch.setChecked(false));
                     sb.show();
                 } else {
-                    Snackbar sb = Snackbar.make(MainActivity.this.findViewById(R.id.rootLayout), "The switch is off now", Snackbar.LENGTH_LONG);
-                    sb.setAction("Undo", v -> mySwitch.setChecked(true));
+                    Snackbar sb = Snackbar.make(MainActivity.this.findViewById(R.id.rootLayout), getResources().getString(R.string.switch_off), Snackbar.LENGTH_LONG);
+                    sb.setAction(getResources().getString(R.string.undo), v -> mySwitch.setChecked(true));
                     sb.show();
                 }
             }
         });
-
-
-        //EditText theText = findViewById(R.id.editText);
-        //String userTyped = theText.getText().toString();
-
     }
 }
