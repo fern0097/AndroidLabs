4b976861ffd2d1aeb8d5381c4a15f81344ed4bd7
Wilker Fernandes de Sousa <fern0097@algonquinlive.com>
2020-09-25 22:22:24 -0400

    Step2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
 
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(v -> {
